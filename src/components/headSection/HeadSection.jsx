01bce23af7034798b8820b923a618d031eddf8ba
veeereshnaik7 <jerryveeru1@gmail.com>
2024-05-20 07:08:30 +0530

    chnages

--- src/components/headSection/HeadSection.jsx
+++ src/components/headSection/HeadSection.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import './style.css';
+import "./style.css";
 const HeadSection = () => {
   return (
     <div className="headSection-outer">
+      <p id="homeShop" style={{ textAlign: "left" }}>
+        {" "}
+        <span style={{ opacity: "0.5" }}>HOME</span> | SHOP
+      </p>
       <h1>DISCOVER OUR PRODUCTS</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus
-        scelerisque.
+        Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus  <br />
+        scelerisque. Dolor integer scelerisque nibh amet mi ut elementum dolor.
       </p>
-      <p>Dolor integer scelerisque nibh amet mi ut elementum dolor.</p>
     </div>
   );
 };
